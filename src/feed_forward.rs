10f8bad797b750ba70f94c1fe1f5dc4b0ca77fab
Michael Nye <thenyeguy@gmail.com>
2017-02-20 14:29:14 -0800

    Operate on in-place on pre-allocated memory

--- src/feed_forward.rs
+++ src/feed_forward.rs
@@ -17,7 +17,8 @@
 //! // Train a network using those examples
 //! let network = Trainer::new(&[2, 3, 1])
 //!     .activator(Activator::ReLU)
-//!     .stop_condition(StopCondition::ErrorThreshold(0.01))
+//!     .logging(Logging::Iterations(100))
+//!     .stop_condition(StopCondition::Iterations(1000))
 //!     .train(&examples[..])
 //!     .unwrap();
 //!
@@ -115,59 +116,44 @@ impl Layer {
     }
 
     /// Feeds the provided `inputs` forward through the layer.
-    fn forward(&self, inputs: &[f64]) -> Vec<f64> {
+    fn forward(&self, inputs: &[f64], outputs: &mut [f64]) {
         assert_eq!(inputs.len(), self.input_len());
-        let mut outputs = vec![0.0; self.output_len()];
+        assert_eq!(outputs.len(), self.output_len());
         f64::gemv(Transpose::NoTrans,
                   &1.0,
                   &self.weights,
                   inputs,
                   &1.0,
-                  &mut outputs);
-        for y in &mut outputs {
+                  outputs);
+        for y in outputs {
             *y = self.activator.f(*y);
         }
-        outputs
     }
 
     /// Feeds the provided `costs` backwards through the layer.
-    ///
-    /// Arguments:
-    ///
-    ///  * `outputs` - the outputs from this layer from the corresponding
-    ///               forward pass.
-    ///  * `costs` - the cost delta from the next layer.
-    ///
-    /// Output:
-    ///   The input cost delta to this layer.
-    fn backward(&self, outputs: &[f64], costs: &[f64]) -> Vec<f64> {
+    fn backward(&self,
+                outputs: &[f64],
+                output_errors: &mut [f64],
+                input_errors: &mut [f64]) {
         assert_eq!(outputs.len(), self.output_len());
-        assert_eq!(costs.len(), self.output_len());
-        let mut deltas = vec![0.0; self.input_len()];
+        assert_eq!(output_errors.len(), self.output_len());
+        assert_eq!(input_errors.len(), self.input_len());
+        for (y, e) in outputs.iter().zip(output_errors.iter_mut()) {
+            *e *= self.activator.fprime(*y);
+        }
         f64::gemv(Transpose::Trans,
                   &1.0,
                   &self.weights,
-                  costs,
+                  output_errors,
                   &1.0,
-                  &mut deltas);
-        for (y, d) in outputs.iter().zip(deltas.iter_mut()) {
-            *d *= self.activator.fprime(*y);
-        }
-        deltas
+                  input_errors);
     }
 
     /// Updates the weights of this layer.
-    ///
-    /// Arguments:
-    ///
-    ///  * `rate` - the learning rate to be used.
-    ///  * `inputs` - the input to this layer from the corresponding forward
-    ///               pass.
-    ///  * `deltas` - the cost delta for this layer's output.
-    fn update(&mut self, rate: f64, inputs: &[f64], deltas: &[f64]) {
+    fn update(&mut self, rate: f64, inputs: &[f64], output_errors: &[f64]) {
         assert_eq!(inputs.len(), self.input_len());
-        assert_eq!(deltas.len(), self.output_len());
-        f64::ger(&(-rate), inputs, deltas, &mut self.weights);
+        assert_eq!(output_errors.len(), self.output_len());
+        f64::ger(&(-rate), inputs, output_errors, &mut self.weights);
     }
 }
 
@@ -207,66 +193,53 @@ impl Network {
     /// layer.
     pub fn run(&self, input: &[f64]) -> Vec<f64> {
         assert_eq!(input.len(), self.layers.front().input_len());
-        self.feed_forward(input).pop().expect("missing layer outputs")
+        let mut network = self.empty_network();
+        self.feed_forward(input, &mut network);
+        network.pop().unwrap()
     }
 
     /// Feeds the provided `input` through the network, returning the activated
     /// values for each layer.
-    fn feed_forward(&self, input: &[f64]) -> Vec<Vec<f64>> {
-        let mut network = Vec::with_capacity(self.layers.len());
-        network.push(input.to_vec());
-        for layer in &self.layers {
-            let output = layer.forward(network.back());
-            network.push(output);
+    fn feed_forward(&self, input: &[f64], network: &mut [Vec<f64>]) {
+        network[0].copy_from_slice(input);
+        for (i, layer) in self.layers.iter().enumerate() {
+            let (input, output) = io_layers(network, i);
+            layer.forward(input, output);
         }
-        network
     }
 
     /// Feeds the provided `expected` value back through the network, returning
     /// the computed cost deltas.
-    ///
-    /// Arguments:
-    ///
-    ///  * `network` - the activated values for each layer in the network. This
-    ///                should be the output of `self.feed_forward(...)`.
-    ///  * `expected` - the expected output of the network given the input.
     fn feed_backwards(&self,
                       network: &[Vec<f64>],
-                      expected: &[f64])
-        -> Vec<Vec<f64>> {
-            let mut deltas = Vec::with_capacity(self.layers.len());
-
-            let mut output_delta = Vec::with_capacity(expected.len());
-            for (o, e) in network.back().iter().zip(expected.iter()) {
-                output_delta.push(o - e);
-            }
-            deltas.push(output_delta);
-
-            for i in (0..self.layers.len()).rev() {
-                let delta = self.layers[i]
-                    .backward(&network[i + 1], &deltas.back());
-                deltas.push(delta);
-            }
-            deltas.reverse();
-            deltas
+                      expected: &[f64],
+                      errors: &mut [Vec<f64>]) {
+        for i in 0..expected.len() {
+            errors.mut_back()[i] = network.back()[i] - expected[i];
         }
+        for (i, layer) in (self.layers.iter().enumerate()).rev() {
+            let (input, output) = io_layers(errors, i);
+            layer.backward(&network[i + 1], output, input);
+        }
+    }
 
     /// Updates the network weights.
-    ///
-    /// Arguments:
-    ///
-    ///  * `rate` - the learning to use for gradient descent.
-    ///  * `network` - the activated values of each layer in the network. This
-    ///                should be the output of `self.feed_forward(...)`.
-    ///  * `deltas` - the cost deltas, computed from `self.feed_backwards(...)`.
     fn update(&mut self,
               rate: f64,
               network: &[Vec<f64>],
-              deltas: &[Vec<f64>]) {
-        for (layer, (delta, input)) in
-            self.layers.iter_mut().zip(deltas[1..].iter().zip(network.iter())) {
-                layer.update(rate, input, delta);
-            }
+              errors: &[Vec<f64>]) {
+        for (i, layer) in self.layers.iter_mut().enumerate() {
+            layer.update(rate, &network[i], &errors[i + 1]);
+        }
+    }
+
+    fn empty_network(&self) -> Vec<Vec<f64>> {
+        let mut network = Vec::new();
+        network.push(vec![0.0; self.layers.front().input_len()]);
+        for layer in &self.layers {
+            network.push(vec![0.0; layer.output_len()]);
+        }
+        network
     }
 }
 
@@ -407,58 +380,71 @@ impl Trainer {
     pub fn train<I, O>(self, examples: &[(I, O)]) -> Result<Network, ()>
         where I: AsRef<[f64]>,
               O: AsRef<[f64]>
-              {
-                  try!(self.validate(examples));
+    {
+        try!(self.validate(examples));
 
-                  let mut network = Network::new(self.activator, &self.layer_sizes);
-                  let mut iteration = 0;
-                  let mut training_error;
-                  loop {
-                      training_error = 0.0;
-                      for &(ref input, ref expected) in examples {
-                          let activations = network.feed_forward(input.as_ref());
-                          let deltas =
-                              network.feed_backwards(&activations, expected.as_ref());
-                          network.update(self.learning_rate, &activations, &deltas);
-                          training_error += mean_square_error(activations.back(),
-                          expected.as_ref());
-                      }
-                      training_error /= 2.0 * examples.len() as f64;
-                      iteration += 1;
-                      self.logging.iteration(iteration, training_error);
-                      if self.stop_condition
-                          .should_stop(iteration, training_error) {
-                              break;
-                          }
-                  }
-                  self.logging.completion(iteration, training_error);
-                  Ok(network)
-              }
+        let mut network = Network::new(self.activator, &self.layer_sizes);
+        let mut activations = network.empty_network();
+        let mut errors = network.empty_network();
+        let mut iteration = 0;
+        let mut training_error;
+        loop {
+            training_error = 0.0;
+            for &(ref input, ref expected) in examples {
+                activations.zero_out();
+                errors.zero_out();
+                network.feed_forward(input.as_ref(), &mut activations);
+                network.feed_backwards(&activations,
+                                       expected.as_ref(),
+                                       &mut errors);
+                network.update(self.learning_rate, &activations, &errors);
+                training_error += mean_square_error(activations.back(),
+                                                    expected.as_ref());
+            }
+            training_error /= 2.0 * examples.len() as f64;
+            iteration += 1;
+            self.logging.iteration(iteration, training_error);
+            if self.stop_condition
+                .should_stop(iteration, training_error) {
+                break;
+            }
+        }
+        self.logging.completion(iteration, training_error);
+        Ok(network)
+    }
 
     /// Verifies that all provided inputs to the `Trainer` are valid, returning
     /// an error if something is wrong.
     fn validate<I, O>(&self, examples: &[(I, O)]) -> Result<(), ()>
         where I: AsRef<[f64]>,
               O: AsRef<[f64]>
-              {
-                  if self.layer_sizes.len() < 3 {
-                      return Err(());
-                  }
-                  for layer_size in &self.layer_sizes {
-                      if *layer_size == 0 {
-                          return Err(());
-                      }
-                  }
-                  for &(ref input, ref output) in examples {
-                      if input.as_ref().len() != *self.layer_sizes.front() {
-                          return Err(());
-                      }
-                      if output.as_ref().len() != *self.layer_sizes.back() {
-                          return Err(());
-                      }
-                  }
-                  Ok(())
-              }
+    {
+        if self.layer_sizes.len() < 3 {
+            return Err(());
+        }
+        for layer_size in &self.layer_sizes {
+            if *layer_size == 0 {
+                return Err(());
+            }
+        }
+        for &(ref input, ref output) in examples {
+            if input.as_ref().len() != *self.layer_sizes.front() {
+                return Err(());
+            }
+            if output.as_ref().len() != *self.layer_sizes.back() {
+                return Err(());
+            }
+        }
+        Ok(())
+    }
+}
+
+/// Gets input and output slices for a layer.
+fn io_layers(layers: &mut [Vec<f64>],
+             layer: usize)
+             -> (&mut [f64], &mut [f64]) {
+    let (before, after) = layers[layer..].split_at_mut(1);
+    (&mut before[0], &mut after[0])
 }
 
 /// Computes the mean squared error between `actual` and `expected`.
@@ -500,6 +486,26 @@ impl<T> Items<T> for [T] {
     fn mut_back(&mut self) -> &mut T {
         let i = self.len() - 1;
         &mut self[i]
+    }
+}
+
+trait ZeroOut {
+    fn zero_out(&mut self);
+}
+
+impl ZeroOut for Vec<f64> {
+    fn zero_out(&mut self) {
+        for elem in self {
+            *elem = 0.0;
+        }
+    }
+}
+
+impl ZeroOut for Vec<Vec<f64>> {
+    fn zero_out(&mut self) {
+        for buffer in self {
+            buffer.zero_out()
+        }
     }
 }
 
